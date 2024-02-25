d19383c669a43795ed87518e01aa5b4a67b0a433
Chukuwinke <rittnerodigwe@gmail.com>
2024-02-25 20:49:22 +0100

    modified cart and sign in on the header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,16 +11,23 @@ export function Header({header, isLoggedIn, cart}) {
   const newMenu = menu.items.filter((_, index) => index < 3 );
   console.log(newMenu)
   return (
-    <header className=" header">
-      <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-        <strong>{"Kangaroo Jane"}</strong>{/*<strong>{shop.name}</strong>*/}
-      </NavLink>
-      <HeaderMenu
-        menu={{ items: newMenu }}
-        viewport="desktop"
-        primaryDomainUrl={header.shop.primaryDomain.url}
-      />
-      <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+    <header className="flex items-center justify-between header">
+      <div>
+        <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
+          <strong>{"Kangaroo Jane"}</strong>{/*<strong>{shop.name}</strong>*/}
+        </NavLink>
+      </div>
+      <div>
+        <HeaderMenu
+          menu={{ items: newMenu }}
+          viewport="desktop"
+          primaryDomainUrl={header.shop.primaryDomain.url}
+        />
+      </div>
+      <div>
+        <HeaderCtas isLoggedIn={isLoggedIn} cart={cart} />
+      </div>
+      
     </header>
   );
 }
@@ -94,7 +101,7 @@ function HeaderCtas({isLoggedIn, cart}) {
       <NavLink prefetch="intent" to="/account" style={activeLinkStyle}>
         <Suspense fallback="Sign in">
           <Await resolve={isLoggedIn} errorElement="Sign in">
-            {(isLoggedIn) => (isLoggedIn ? 'Account' : 'Sign in')}
+            {(isLoggedIn) => (isLoggedIn ? 'Account' : <UserIcon />)}
           </Await>
         </Suspense>
       </NavLink>
@@ -104,6 +111,14 @@ function HeaderCtas({isLoggedIn, cart}) {
   );
 }
 
+// User icon component
+function UserIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+    </svg>
+  );
+}
 function HeaderMenuMobileToggle() {
   return (
     <a className="header-menu-mobile-toggle" href="#mobile-menu-aside">
@@ -120,7 +135,24 @@ function SearchToggle() {
  * @param {{count: number}}
  */
 function CartBadge({count}) {
-  return <a href="#cart-aside">Cart {count}</a>;
+  return( <a href="#cart-aside" className="relative"><svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  strokeWidth={1.5}
+  stroke="currentColor"
+  className="w-6 h-6 mr-1"
+>
+  <path
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+  />
+</svg>
+<span className="absolute top-0 right-0 transform translate-x-1/4 translate-y-1/10 bg-red-500 text-white rounded-full w-4 h-4 flex justify-center items-center -mt-1 -mr-1">
+    {count}
+</span>
+</a>);
 }
 
 /**
